8d715377d36f3d00f787cf13cb96caad84ddbe70
samuel.hangouet <samuel.hangouet@d5d8a9c8-1d32-11de-82b5-194b17410922>
2009-03-30 15:44:26 +0000

    add scripts

---
+++ Script/make_package.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ ! "$*" ]; then
+    echo Please, give a list of modules you want to package
+    exit 1
+fi
+
+pkg="Ell-`date +%Y%b%d`"
+echo Generate $pkg
+mkdir $pkg
+cp --parent CommonHeaders.h COPYING.LESSER Makefile $pkg/
+cp --parent `find Script -name "*.mk"` $pkg/
+for m in $*; do
+    cp --parent `find $m/Include -name "*.h"` $pkg/
+    cp --parent `find $m/Source $m/Test -name "*.cpp"` $pkg/
+    cp --parent $m/Doc/* $pkg/
+    cp --parent $m/Makefile $pkg/
+done
+
+tar cjvf $pkg.tar.bz2 $pkg
+rm -rf $pkg
