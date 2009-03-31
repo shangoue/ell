dccdbc8f54202468a56466b5907b04d875b2383a
samuel.hangouet <samuel.hangouet@d5d8a9c8-1d32-11de-82b5-194b17410922>
2009-03-31 08:55:40 +0000

    update script to get snapshot of the doc in the package

--- Script/make_package.sh
+++ Script/make_package.sh
@@ -5,18 +5,26 @@ if [ ! "$*" ]; then
     exit 1
 fi
 
-ver=`svnversion | sed "s/^[^:]*:\([0-9M]\+\)/\1/"`
+svn up
+ver=`svnversion | sed "s/^[^:]*:\([0-9]\+\)/\1/" | sed "s/\([0-9]\+\)M\?/\1/"`
+
 pkg="Ell-`date +%Y%b%d`-r$ver"
 echo Generate $pkg
 mkdir $pkg
+
 cp --parent CommonHeaders.h COPYING.LESSER Makefile $pkg/
 cp --parent `find Script -name "*.mk"` $pkg/
-for m in $*; do
-    cp --parent `find $m/Include -name "*.h"` $pkg/
-    cp --parent `find $m/Source $m/Test -name "*.cpp"` $pkg/
-    cp --parent $m/Doc/* $pkg/
+for m in libELL $*; do
     cp --parent $m/Makefile $pkg/
+    for dir in Source Test Include; do
+        [ -d $m/$dir ] && cp --parent `find $m/$dir -name "*.cpp" -o -name "*.h"` $pkg/
+    done
 done
+
+pushd ../wiki
+svn up
+popd
+cp ../wiki/ReferenceManual.wiki $pkg/
 
 tar cjvf $pkg.tar.bz2 $pkg
 rm -rf $pkg
