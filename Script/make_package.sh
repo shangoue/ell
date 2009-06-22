822c088fddecab70be2714e7167e58c6458adef3
samuel.hangouet <samuel.hangouet@d5d8a9c8-1d32-11de-82b5-194b17410922>
2009-06-22 12:10:35 +0000

    fix gcc limitation

--- Script/make_package.sh
+++ Script/make_package.sh
@@ -15,7 +15,7 @@ mkdir $pkg/include
 
 cp -v --parent COPYING.LESSER $pkg/ || exit 1
 
-make -j 5 MODE=Release || exit 1
+make MODE=Release || exit 1
 
 # Run non-regression test-suite
 for test in `find Build -name *_test`;
